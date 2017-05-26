083d0c3f8b2822c6237a7ab6a86a43ab58ccaf0f
Nicholas Ver Voort <moldorma@gmail.com>
2017-05-25 23:08:40 -0400

    add copyright to new file

--- corona-ide-ui/src/main/java/com/coronaide/ui/custom/AlertWithCheckbox.java
+++ corona-ide-ui/src/main/java/com/coronaide/ui/custom/AlertWithCheckbox.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) May 25, 2017 Corona IDE.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    nickavv - initial API and implementation and/or initial documentation
+ */
 package com.coronaide.ui.custom;
 
 import javafx.scene.Group;
