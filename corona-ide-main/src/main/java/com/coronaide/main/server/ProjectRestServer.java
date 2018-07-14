1b243a08a6ed8592ed46b64a06f8567425c28cad
Nicholas Ver Voort <nvervoort@blackducksoftware.com>
2018-07-14 13:24:38 -0400

    #71 : add project rest server

---
+++ corona-ide-main/src/main/java/com/coronaide/main/server/ProjectRestServer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Jul 14, 2018 StarChart Labs Authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    nickv - initial API and implementation and/or initial documentation
+ */
+package com.coronaide.main.server;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.inject.Inject;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coronaide.core.model.Project;
+import com.coronaide.core.model.ProjectDeleteRequest;
+import com.coronaide.core.model.ProjectRequest;
+import com.coronaide.core.service.IProjectService;
+
+@RestController
+public class ProjectRestServer {
+
+	private final IProjectService projectService;
+	
+	@Inject
+	public ProjectRestServer(IProjectService projectService) {
+		this.projectService = projectService;
+	}
+	
+	@RequestMapping(value = "/projects", method = RequestMethod.GET)
+	public Collection<Project> getAllProjects() {
+		return projectService.getAll();
+	}
+	
+	@RequestMapping(value = "/projects", method = RequestMethod.POST)
+	public Project createProject(@RequestBody ProjectRequest request) throws IOException {
+		return projectService.create(request);
+	}
+	
+	@RequestMapping(value = "/projects", method = RequestMethod.DELETE)
+    public void deleteProject(@RequestBody ProjectDeleteRequest request) throws IOException {
+        projectService.delete(request);
+	}
+	
+}
